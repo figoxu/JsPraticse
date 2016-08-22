109b85f1d8cec270fce080a55f17a9fdbe8940ba
Figo.Xu <xujh945@qq.com>
2016-08-22 18:00:05 +0800

    add css require

--- webpack/1st/command.bat
+++ webpack/1st/command.bat
@@ -2,3 +2,6 @@ npm install webpack -g
 npm install webpack --save-dev   [ 安装指定版本： npm install webpack@1.12.x --save-dev   ]
 npm info webpack
 npm install webpack-dev-server --save-dev
+
+
+npm install css-loader style-loader
--- webpack/1st/entry.js
+++ webpack/1st/entry.js
@@ -1,2 +1,3 @@
+require("!style!css!./style.css")
 document.write('It works.')
 document.write(require('./module.js'))
