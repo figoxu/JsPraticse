c1139ae90ba2ab634fbaecedc5e63c7bb76eb00e
Figo.Xu <xujh945@qq.com>
2016-08-26 16:17:42 +0800

    requrejs another way

---
+++ requireJs/world/dist/js/hi.js
@@ -0,0 +1,9 @@
+function hello(){
+    document.body.style.backgroundColor="#E0EEEE"
+}
+
+function hi(){
+    var element = document.createElement("h1");
+    element.innerHTML="Hello world"
+    document.body.appendChild(element);
+}
