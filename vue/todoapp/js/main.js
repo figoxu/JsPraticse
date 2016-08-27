f5b8bef0b270d4badd869e5cdfa7752d8a658817
Figo.Xu <xujh945@qq.com>
2016-08-27 11:34:39 +0800

    add ToDo Component

--- vue/todoapp/js/main.js
+++ vue/todoapp/js/main.js
@@ -1,37 +1,56 @@
+var TodoComponent = Vue.extend({
+    data: function () {
+        return {
+            inEditMode: false
+        }
+    },
+    props:['todo'],
+    template: '<div><span v-show="!inEditMode" @click="clicked">{{ todo.title }}</span><input v-show="inEditMode" @keyup.enter="saved" type="text" v-model="todo.title"/></div>',
+    methods: {
+        clicked: function () {
+            this.inEditMode = true;
+        },
+        saved:function(){
+            this.inEditMode = false;
+        }
+    }
+});
+
+Vue.component("todo-component", TodoComponent);
 
 new Vue({
-    el:"#app",
-    data:{
-        message:'Hello Vue Todo App',
-        someLink:'http://www.google.com.hk',
-        todos:[
-            {title:'Todo 1'},
-            {title:'Todo 2'},
-            {title:'Todo 3'},
-            {title:'Todo 4'},
-            {title:'Todo 5'}
+    el: "#app",
+    data: {
+        message: 'Hello Vue Todo App',
+        someLink: 'http://www.google.com.hk',
+        todos: [
+            {title: 'Todo 1'},
+            {title: 'Todo 2'},
+            {title: 'Todo 3'},
+            {title: 'Todo 4'},
+            {title: 'Todo 5'}
         ],
-        showTodos:true,
-        todoText:"",
-        count:0
+        showTodos: true,
+        todoText: "",
+        count: 0
     },
-    methods:{
-        createTodo:function (){
+    methods: {
+        createTodo: function () {
             console.log("enter")
             var todoText = this.todoText.trim();
-            if( todoText ){
-                this.todos.push({title:todoText});
+            if (todoText) {
+                this.todos.push({title: todoText});
                 this.todoText = "";
             }
         },
-        clearTodo:function(){
+        clearTodo: function () {
             this.todoText = '';
         },
-        increaseCount:function(){
+        increaseCount: function () {
             this.count++;
         },
-        removeTodo:function(index){
-            this.todos.splice(index,1);
+        removeTodo: function (index) {
+            this.todos.splice(index, 1);
         }
     }
 });
