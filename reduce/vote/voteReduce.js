19fb2f89774d8e01e695c3aaad4a0e3897ab4f16
Figo.Xu <xujh945@qq.com>
2016-10-04 16:09:08 +0800

    Reducing an Array into an Object

---
+++ reduce/vote/voteReduce.js
@@ -0,0 +1,24 @@
+var votes=[
+    "angular",
+    "angular",
+    "react",
+    "react",
+    "react",
+    "angular",
+    "ember",
+    "react",
+    "vanillac"
+];
+var initialValue={};
+var reducer = function(tally,vote){
+    if(!tally[vote]){
+        tally[vote]=1
+    }else{
+        tally[vote]=tally[vote]+1;
+    }
+    return tally;
+};
+var result = votes.reduce(reducer,initialValue);
+console.log(result)
+
+
