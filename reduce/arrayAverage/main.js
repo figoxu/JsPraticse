4e8e1e438474f9a3479983b448a51e00f199f028
Figo.Xu <xujh945@qq.com>
2016-10-05 07:52:04 +0800

    Add Reduce From Right

--- reduce/arrayAverage/main.js
+++ reduce/arrayAverage/main.js
@@ -1,4 +1,5 @@
 function reducer(accumulator,value,index,array){
+    console.log("Value From Left:",value)
     var intermediaryValue = accumulator + value;
     if( index === array.length-1 ){
         return intermediaryValue / array.length;
@@ -8,4 +9,15 @@ function reducer(accumulator,value,index,array){
 
 var data = [1,2,3,3,4,5,3,1]
 var average = data.reduce(reducer,0);
-console.log(average)
+console.log(average);
+
+
+var average = data.reduceRight(function(accumulator,value,index,array){
+    console.log("Value From Right:",value)
+    var intermediaryValue = accumulator + value;
+    if( index === 0 ){
+        return intermediaryValue / array.length;
+    }
+    return intermediaryValue;
+},0);
+console.log(average);
