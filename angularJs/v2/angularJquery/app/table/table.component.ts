b51f281f86e2511140350bde51ff26cfeb657cc1
Figo.Xu <xujh945@qq.com>
2016-12-22 18:18:53 +0800

    Add Jquery Angular Sample

--- angularJs/v2/angularJquery/app/table/table.component.ts
+++ angularJs/v2/angularJquery/app/table/table.component.ts
@@ -1,10 +1,32 @@
 import {Component } from '@angular/core';
 import {Router} from '@angular/router';
+declare var jQuery:any;
 @Component({
     selector:'table',
     templateUrl:'app/table/table.html'
 })
 
 export class TableComponent {
+
+    ngOnInit():any{
+        var $ = jQuery;
+        $('#buildTable').bind('click', function(e) {
+            e.preventDefault();
+            $('#tablePlaceholder').empty().buildTable();
+        });
+
+        $('#runPlugin').bind('click', function(e) {
+            e.preventDefault();
+            var confParam = {
+                height: '400'
+                , altClass: 'odd'
+                , themeClass: 'fancyDarkTable'
+                ,fixedColumns:1
+                ,fixedColumn:true
+            };
+            $('#tablePlaceholder table').fixedHeaderTable(confParam);
+        });
+    }
+
 }
 
