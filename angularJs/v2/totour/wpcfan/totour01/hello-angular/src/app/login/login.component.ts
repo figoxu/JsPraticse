2830958b686a642fddbbb3061f7f1f088028c8b7
figoxu <xujh945@gmail.com>
2017-01-17 22:20:46 +0800

    User MVVM For Input Element

--- angularJs/v2/totour/wpcfan/totour01/hello-angular/src/app/login/login.component.ts
+++ angularJs/v2/totour/wpcfan/totour01/hello-angular/src/app/login/login.component.ts
@@ -8,9 +8,11 @@ import {AuthService} from "../auth.service";
       login Works!
     </p>
     <div>
-    <input #usernameRef type="text">
-    <input #passwordRef type="password">
-    <button (click)="onClick(usernameRef.value,passwordRef.value)">Login</button>
+    <input #usernameRef="ngModel" [(ngModel)]="username" type="text" required minlength="3">
+    {{usernameRef.valid}}
+    <input #passwordRef="ngModel" [(ngModel)]="password" type="password" required minlength="3">
+    {{passwordRef.valid}}
+    <button (click)="onClick(usernameRef.value,passwordRef.va)">Login</button>
 </div>
   `,
   styles: []
@@ -18,15 +20,17 @@ import {AuthService} from "../auth.service";
   providers:[AuthService]
 })
 export class LoginComponent implements OnInit {
+  username=""
+  password=""
 
   constructor(private service:AuthService) { }
 
   ngOnInit() {
   }
 
-  onClick(username,password){
-    var v = this.service.login(username,password)
-    console.log("username:",username," password:",password," @result:",v)
+  onClick(){
+    var v = this.service.login(this.username,this.password)
+    console.log("username:",this.username," password:",this.password," @result:",v)
   }
 
 }
