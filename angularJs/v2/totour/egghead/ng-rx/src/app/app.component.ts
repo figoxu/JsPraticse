a6242753ecebb8c327ad6b2dde8d13e795c8d611
figoxu <xujh945@gmail.com>
2017-01-21 06:36:42 +0800

    rxjs : Managing State With StartWith And Scan

--- angularJs/v2/totour/egghead/ng-rx/src/app/app.component.ts
+++ angularJs/v2/totour/egghead/ng-rx/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {Observable} from 'rxjs/Observable'
 import 'rxjs/add/Observable/interval'
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/startWith'
+import 'rxjs/add/operator/scan'
 import {Subject} from "rxjs/Rx";
 
 @Component({
@@ -17,7 +19,12 @@ export class AppComponent {
     this.clock = Observable.merge(
       this.click$,
       Observable.interval(5000)
-    ).map(()=>new Date());
+    ).startWith(new Date())
+      .scan((acc:Date,curr)=>{
+        const date = new Date(acc.getTime());
+        date.setSeconds(date.getSeconds()+1)
+        return date;
+      });
   }
 
 
