1c0dc5b7453ae4ccdb6315e53383e1fa5c7d509f
figoxu <xujh945@gmail.com>
2017-01-26 21:08:42 +0800

    vue: components communicate with  prop and event

--- vue/vue_2/vue-cli/HelloVueCli/src/components/Input.vue
+++ vue/vue_2/vue-cli/HelloVueCli/src/components/Input.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
-    <input type="text" v-model=" message ">
+    <p>{{ msg }}</p>
+    <input type="text" v-model="msg" @input="changeMsg">
     <p>{{ message }}</p>
   </div>
 </template>
 
 <script>
   export default{
+    props: ['msg'],
     data () {
       return {
         message: ''
+      }
+    },
+    methods: {
+      changeMsg (event) {
+        this.message = event.target.value
+        this.$emit('msgChanged', this.message)
       }
     }
   }
--- vue/vue_2/vue-cli/HelloVueCli/src/components/Message.vue
+++ vue/vue_2/vue-cli/HelloVueCli/src/components/Message.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-    <h1>This is a great Message !</h1>
-    <app-input></app-input>
+    <h1>{{ message }}</h1>
+    <app-input :msg="message"  @msgChanged="message = $event"></app-input>
   </div>
 </template>
 <script>
   import Input from './Input.vue'
 
   export default {
+    data () {
+      return {
+        message: 'This is a great Message !'
+      }
+    },
     components: {
       'app-input': Input
     }
