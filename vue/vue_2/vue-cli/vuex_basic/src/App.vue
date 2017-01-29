b3201a20f5b66f445f0cfdd50b712cf0372b4455
figoxu <xujh945@gmail.com>
2017-01-29 10:50:20 +0800

    vuex sample

--- vue/vue_2/vue-cli/vuex_basic/src/App.vue
+++ vue/vue_2/vue-cli/vuex_basic/src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <app-registration @userRegistered="userRegistered" :users="unregisteredUsers">
+
+    </app-registration>
+
+    <app-registrations @userUnregistered="userUnregistered" :registrations="registrations">
+
+    </app-registrations>
   </div>
 </template>
 
+
 <script>
-import Hello from './components/Hello'
+  import Registration from './components/Registration'
+  import Registrations from './components/Registrations'
 
-export default {
-  name: 'app',
-  components: {
-    Hello
+  export default {
+    data () {
+      return {
+        registrations:[],
+        users : [
+          {id:1, name:'Max',registered:false},
+          {id:2,name:'Anna',registered:false},
+          {id:3,name:'Chris',registered:false},
+          {id:4,name:'Sven',registered:false}
+        ]
+      }
+    },
+    computed:{
+      unregisteredUsers(){
+        return this.users.filter((user)=>{
+          return !user.registered;
+        })
+      }
+    },
+    methods:{
+      userRegistered(user){
+        const date = new Date();
+        console.log("@user",user.id)
+        this.registrations.push({
+          userId:user.id,
+          name:user.name,
+          date:date.getMonth()+'/'+date.getDay()
+        })
+
+
+      }
+      ,userUnregistered(registration){
+        this.users.find(u=>{
+          if ( u.id == registration.userId){
+            u.registered = false;
+          }
+          console.log(u);
+          return false
+        })
+        this.registrations.splice(this.registrations.indexOf(registration),1);
+      }
+    },
+    components:{
+      appRegistration:Registration,
+      appRegistrations:Registrations
+    }
   }
-}
-</script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+</script>
