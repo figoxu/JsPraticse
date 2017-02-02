c1f1c8556f69ec8f4c7e571658af94143c4f4428
figoxu <xujh945@gmail.com>
2017-02-02 09:23:52 +0800

    Calendar Sample

--- vue/vue_2/vue-cli/fullcalendar/src/components/Hello.vue
+++ vue/vue_2/vue-cli/fullcalendar/src/components/Hello.vue
@@ -1,53 +1,72 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div>
+    <p>
+      Here is the demo of vue-fullcalendar, no jquery fullcalendar.js required!
+    </p>
+    <full-calendar class="test-fc" :events="fcEvents"
+                   first-day='1' lang="en"
+                   @changeMonth="changeMonth"
+                   @eventClick="eventClick"
+                   @dayClick="dayClick"
+                   @moreClick="moreClick">
+    </full-calendar>
   </div>
 </template>
-
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import Vue from 'vue'
+  import fullCalendar from 'vue-fullcalendar'
+
+  Vue.component('full-calendar', fullCalendar)
+
+  let demoEvents = [
+    {
+      title    : 'Sunny 725-727',
+      start    : '2017-01-01',
+      end      : '2017-02-27',
+      cssClass : 'family'
+    },
+    {
+      title : 'Sunny 726-727',
+      start : '2017-02-26',
+      end : '2017-02-27',
+      cssClass : ['home', 'work']
+    },
+    {
+      title : 'Sunny 727-728',
+      start : '2017-02-27',
+      end : '2017-02-28'
+    },
+    {
+      title : 'Sunny 728-730',
+      start : '2017-02-28',
+      end : '2017-03s-30'
+    },
+  ]
+  export default {
+    data () {
+      return {
+        name:'Sunny!',
+        fcEvents : demoEvents
+      }
+    },
+    methods : {
+      'changeMonth' (start, end, current) {
+        console.log('changeMonth', start, end, current)
+      },
+      'eventClick' (event, jsEvent, pos) {
+        console.log('eventClick', event, jsEvent, pos)
+      },
+      'dayClick' (day, jsEvent) {
+        console.log('dayClick', day, jsEvent)
+      },
+      'moreClick' (day, events, jsEvent) {
+        console.log('moreCLick', day, events, jsEvent)
+      }
     }
   }
-}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style>
+  .app{
+    color:green;
+  }
 </style>
